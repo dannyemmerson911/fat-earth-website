ec31b4b09bd7f17d2083c12c4f1cc214af43432e
Danny Emmerson <danny.emmerson911@gmail.com>
2018-05-11 20:05:22 -0400

    some links

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 (function(){
-    
+    $('.pageToggle').hide();
+    $('.homePage').show();
+    $('.home').on('click', function(){
+        $('.pageToggle').hide();
+        $('.homePage').show();
+    })
+    $('.tvNews').on('click', function(){
+        $('.pageToggle').hide();
+        $('.tvNewsPage').show(); 
+    })
 }());
